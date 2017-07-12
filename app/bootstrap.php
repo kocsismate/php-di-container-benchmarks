5df34afab47d56f7cb60de7a552af91533ec687e
kocsis.mate <kocsismate@woohoolabs.com>
2017-07-12 20:45:01 +0200

    Fix Disco results in when running the benchmark in Docker

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types=1);
 
-define("PROJECT_ROOT", __DIR__ . "/..");
+define("PROJECT_ROOT", dirname(__DIR__));
 
 require_once(PROJECT_ROOT . "/vendor/autoload.php");
--- build/container/fpm/run.sh
+++ build/container/fpm/run.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+chmod -R 777 /code/src/Container/Disco/Resource
+
 cp /code/build/container/fpm/custom-php.ini /usr/local/etc/php/conf.d/zz-custom-php.ini
 
 sed -i "s/OPCACHE_ENABLED/$OPCACHE_ENABLED/g" /usr/local/etc/php/conf.d/zz-custom-php.ini
