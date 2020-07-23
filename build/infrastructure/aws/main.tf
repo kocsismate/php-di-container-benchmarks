87311edb913b90bd2ec5490db1fd8cd09624c3c8
Máté Kocsis <kocsismate@woohoolabs.com>
2020-07-23 23:12:41 +0200

    Ignore composer platform requirements on AWS as well

--- build/infrastructure/aws/main.tf
+++ build/infrastructure/aws/main.tf
@@ -85,7 +85,7 @@ EOF
       "sudo cp .env.dist .env",
 
       "# Install composer dependencies",
-      "sudo docker run --rm --interactive --tty --volume $PWD:/code composer install --prefer-dist --no-dev --no-suggest --no-interaction --working-dir=/code",
+      "sudo docker run --rm --interactive --tty --volume $PWD:/code composer install --prefer-dist --no-dev --no-suggest --no-interaction --working-dir=/code --ignore-platform-reqs",
 
       "# Build DI containers",
       "sudo docker-compose run --no-deps benchmark-fpm /code/bin/benchmark build",
